b68f2b68ce8e6e7f36a0dee74902558ad0e3d55f
Johnny Zhu <johhnnnnyy@gmail.com>
2023-03-31 16:58:31 -0700

    minor changes

--- project/static/js/yourcourses.js
+++ project/static/js/yourcourses.js
@@ -1,16 +1,14 @@
-function getAllGrades() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.open("GET", "http://127.0.0.1:5000/coursetest");
-    xhttp.onload = function() {
-        format(this.responseText);
-    }; 
-    xhttp.send();
-}
+var xhttp = new XMLHttpRequest();
+xhttp.open("GET", "http://127.0.0.1:5000/coursetest");
+xhttp.onload = function() {
+    format(this.responseText);
+}; 
+xhttp.send();
+
 function format(data) {
     let parsed = JSON.parse(data);
     let table = "<table><tr><th>Course Name</th><th>Teacher</th><th>Time</th><th>Student Enrolled</th><th>Add Class</th></tr>";
     for (let i = 0; i < parsed.length; i++) {
-        //table += "<tr><td>" + parsed[i].name + "</td><td>" + parsed[i].grade + "</td></tr>";
         table += "<tr><td>" + parsed[i].name + "</td><td>" + parsed[i].teacher + "</td><td>" + parsed[i].time + "</td><td>" + parsed[i].students + "</td><td><button type='button' class='btn btn-primary'>Add</button></td></tr>";
     }
     table += "</table>";

db5676065b1a6473db1c3ce0871b54e3fb8294e9
Nathan Estrada <naestrada19@gmail.com>
2023-04-05 19:42:14 -0700

    Fixed admin view to support editing enrolled courses

--- project/admin.py
+++ project/admin.py
@@ -7,13 +7,15 @@ from . import db
 
 
 class AdminView(ModelView):
+    column_hide_backrefs = False
+    column_list = ('email', 'courses', 'role', 'user_id')
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.static_folder = 'static'
 
     def is_accessible(self):
-        return current_user.is_authenticated
+        return current_user.is_authenticated and current_user.role == Role.ADMIN
 
     def inaccessible_callback(self, name, **kwargs):
         if not self.is_accessible():
